792a371d8cf13317c8c5a6fba9f3adf15c570637
phamin2001 <amin.pahlavani@gmail.com>
2019-06-26 17:34:36 -0600

    setup

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-test('should ', () => {
-    
-})
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
